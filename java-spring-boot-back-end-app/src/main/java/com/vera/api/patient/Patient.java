5132ba1a0028c0dd28ef60067eb8353dd7c7b872
Marcandy <marcandy.nj@gmail.com>
2026-09-10 11:09:59 -0400

    base patient model entity and repository

---
+++ java-spring-boot-back-end-app/src/main/java/com/vera/api/patient/Patient.java
@@ -0,0 +1,42 @@
+package com.vera.api.patient;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
